4248f11ac984c54b34c92bbd7f34873ad003d4e5
Antony Salas <salasra1795@gmail.com>
2026-05-09 12:22:47 -0500

    fix: update html and fix css styles

--- assets/js/utils/header.js
+++ assets/js/utils/header.js
@@ -1,17 +1,17 @@
 const header = () => {
     const header = document.getElementById("header");
     header.innerHTML = `
-    <header class="main-header glass-bar">
-    <a href="index.html" class="header-title">
-        <i class="fa-solid fa-layer-group"></i>Dashboard
-    </a>
-    <nav class="header-nav">
-        <a href="clock.html"><i class="fa-regular fa-clock"></i>Clock</a>
-        <a href="weather.html"><i class="fa-solid fa-cloud-sun"></i>Weather</a>
-        <a href="links.html"><i class="fa-solid fa-link"></i>Links</a>
-        <a href="password.html"><i class="fa-solid fa-lock"></i>Password</a>
-    </nav>
-    </header>
+        <header class="main-header glass-bar">
+        <a href="index.html" class="header-title">
+            <i class="fa-solid fa-layer-group"></i>Dashboard
+        </a>
+        <nav class="header-nav">
+            <a href="digital-clock.html"><i class="fa-regular fa-clock"></i> Clock </a>
+            <a href="weather-station.html"><i class="fa-solid fa-cloud-sun"></i> Weather </a>
+            <a href="favorites-links.html"><i class="fa-solid fa-link"></i> Links </a>
+            <a href="generate-password.html"><i class="fa-solid fa-lock"></i> Password </a>
+        </nav>
+        </header>
     `;
 }
 
