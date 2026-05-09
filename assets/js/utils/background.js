1292bf1220a36ea270a5e31faf1e1747633aea35
Antony Salas <salasra1795@gmail.com>
2026-05-09 12:33:18 -0500

    fix: pre-load images for random background

--- assets/js/utils/background.js
+++ assets/js/utils/background.js
@@ -1,7 +1,13 @@
 const randomBackground = () => {
     const body = document.getElementById("body");
-    const images = ["main-bg-1.jpg", "main-bg-2.jpg", "main-bg-3.jpg", "main-bg-4.jpg", "main-bg-5.jpg"];
+    const images = ["main-bg-1.jpg", "main-bg-2.jpg", "main-bg-3.jpg", "main-bg-4.jpg", "main-bg-5.jpg",
+                    "main-bg-6.jpg", "main-bg-7.jpg", "main-bg-8.jpg", "main-bg-9.jpg", "main-bg-10.jpg"];
 
+    //pre-load images
+    images.forEach(img => {
+        const preload = new Image();
+        preload.src = `../../assets/images/${img}`;
+    });
     //initial background
     let i = Math.floor(Math.random() * images.length);
     body.style.background = `url("../../assets/images/${images[i]}") no-repeat center / cover fixed`;
@@ -10,7 +16,7 @@ const randomBackground = () => {
     setInterval(() => {
         i = Math.floor(Math.random() * images.length);
         body.style.background = `url("../../assets/images/${images[i]}") no-repeat center / cover fixed`;
-    }, 15000);
+    }, 3000);
 }
 
 export { randomBackground };
